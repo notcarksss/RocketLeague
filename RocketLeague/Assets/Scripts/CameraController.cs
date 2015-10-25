f3a132018719468a3ea491f734c85ab194288ec5
Eric Provencher <eprovencher92@gmail.com>
2015-10-25 12:23:38 -0400

    Bunch of assets added + basic setup working

---
+++ RocketLeague/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField]
+    GameObject player;
+
+    float stepOver;
+    float mSpeed;
+
+    void Start()
+    {
+        mSpeed = 5.0f;
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+        TranslateToPlayer();
+    }
+
+    void TranslateToPlayer()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+
+        transform.Translate(direction * mSpeed);
+
+    }
+}
---
+++ RocketLeague/Assets/Scripts/ShipController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipController : MonoBehaviour {
+
+    Rigidbody rb;
+
+    float mSpeed;
+    float mRotationSpeed;
+    Vector3 mDirection;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+        mSpeed = 30;
+        mRotationSpeed = 75;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        InputHandling();
+    }
+
+    void ApplyForce(Vector3 force)
+    {
+
+    }
+
+
+    void InputHandling()
+    {
+        float translation = Input.GetAxis("Vertical") * mSpeed;
+        float rotation = Input.GetAxis("Horizontal") * mRotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        mDirection = new Vector3(0, rotation, translation);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "Ball")
+        {
+            other.rigidbody.AddForceAtPosition(50 * mDirection, transform.position, ForceMode.Impulse);
+        }
+    }
+}
