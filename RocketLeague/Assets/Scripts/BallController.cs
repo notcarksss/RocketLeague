4f21b31ad2126262dbd637f4b8ff39aac6ce4253
Eric Provencher <eprovencher92@gmail.com>
2015-11-02 13:21:24 -0500

    More work on flipping

--- RocketLeague/Assets/Scripts/BallController.cs
+++ RocketLeague/Assets/Scripts/BallController.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class BallController : MonoBehaviour {
     Rigidbody rb;
 
-    float bouncyness = 1.1f;
+    float bouncyness = 1.3f;
 
 	// Use this for initialization
 	void Start () {
@@ -15,12 +15,9 @@ public class BallController : MonoBehaviour {
     {
         if(coll.gameObject.tag == "Player")
         {
-            rb.velocity += coll.gameObject.GetComponent<Rigidbody>().velocity;
-        }
-        else
-        {
-            rb.velocity = new Vector3(rb.velocity.x, coll.relativeVelocity.y / bouncyness, rb.velocity.z);
-        }        
+            rb.velocity += 10* coll.gameObject.GetComponent<Rigidbody>().velocity;
+        }       
+        rb.velocity = new Vector3(rb.velocity.x, coll.relativeVelocity.y / bouncyness, rb.velocity.z);                
     }
 
 }
--- RocketLeague/Assets/Scripts/ShipController.cs
+++ RocketLeague/Assets/Scripts/ShipController.cs
@@ -20,6 +20,18 @@ public class ShipController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         InputHandling();
+        CheckIfScrewedUpAngle();
+    }
+
+    void CheckIfScrewedUpAngle()
+    {
+        //FipOver
+
+        if(transform.rotation.z > 1 && transform.position.y < 6)
+        {
+            Debug.Log("FLIP");
+            transform.Rotate(Vector3.forward, 180);
+        }
     }
 
     void ApplyForce(Vector3 force)
@@ -38,15 +50,21 @@ public class ShipController : MonoBehaviour {
         transform.Rotate(0, rotation, 0);
 
         mDirection = new Vector3(0, rotation, translation);
+
+        if(Input.GetKey("space"))
+        {
+            Debug.Log("O)Ut");
+            rb.AddForce(100 * Vector3.up);
+        }
     }
 
     void OnCollisionEnter(Collision other)
     {
-       /*
+       
         if (other.gameObject.name == "Ball")
         {
-            other.rigidbody.AddForceAtPosition(100 * mDirection, transform.position, ForceMode.Impulse);
+            other.rigidbody.AddForceAtPosition(10 * mDirection, transform.position, ForceMode.Impulse);
         }
-        */
+        
     }
 }
